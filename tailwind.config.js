11fd2c4370cb3fb0bfc452e2e026615893adeab8
NafisNoorNabil <nafisnoornabil29@gmail.com>
2024-03-13 11:59:08 +0600

    modified css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*html'],
+  content: ['./*.html'],
   theme: {
-    screens: {
-      sm:'480px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px'
-    },
     extend: {
+      dropShadow: {
+        '4xl': [
+          '1px 0.5px 1px #495057',
+        ]
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
       fontFamily: {
-        nunito:["Nunito Sans"],
-        oswald:["Oswald"],
-        roboto:["Roboto"],
-        playfair:["Playfair Display"],
-        poppins:["Poppins"],
+        nunito: ["Nunito Sans"],
+        oswald: ["Oswald"],
+        roboto: ["Roboto"],
+        playfair: ["Playfair Display"],
+        poppins: ["Poppins"],
         sfui: ['SF UI', 'sans-serif']
       },
-      letterSpacing:{
-        widest:'.3em'
+      letterSpacing: {
+        widest: '.3em'
       }
     }
   },
   plugins: [
-    require('tailwind-scrollbar'),
+    require('tailwind-scrollbar')
   ],
-}
-
+};
